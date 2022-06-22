084693ca69c79536db723870dc60324f5c7dc193
Alistair Spragg <alistair.spragg@gmail.com>
2022-06-22 19:24:49 +0100

    Minor change so accessed data isnt send on update

--- URLShortener/DAL/Models/ShortLink.cs
+++ URLShortener/DAL/Models/ShortLink.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using SQLite;
 
 namespace UrlShortener.DAL.Models;
@@ -10,7 +11,9 @@ public class ShortLink
     public string RemoteUrl { get; set; } = null!;
     [Indexed] 
     public string ShortCode { get; set; } = null!;
+    [JsonIgnore]
     public string? AccessedSerialised { get; set; }
     [Ignore] 
+    [JsonIgnore]
     public List<DateTime> Accessed => AccessedSerialised == null ? new List<DateTime>() : JsonSerializer.Deserialize<List<DateTime>>(AccessedSerialised)!;
 }
